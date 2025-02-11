168524c53b53ac70513e8bc5b475c50414b2fbe9
Pulkit Jain <142732659+Pullkitt@users.noreply.github.com>
2025-02-12 00:47:40 +0530

    Update SuperClass.java

--- src/exp5/SuperClass.java
+++ src/exp5/SuperClass.java
@@ -1,5 +1,25 @@
 package exp5;
 
 public class SuperClass {
+		    private int numprivate = 6;
 
-}
+		    public int getPrivatenum() {
+		        return numprivate;
+		    }
+		}
+
+		class ChildClass extends SuperClass {
+		    public void display() {
+		        // Trying to access privateMember directly will cause an error
+			        System.out.println("Private member value: " + getPrivatenum());
+		    
+		}
+
+		public class Main {
+		    public static void main(String[] args) {
+		        ChildClass obj = new ChildClass();
+		        obj.display();
+		    }
+		}
+	}
+
