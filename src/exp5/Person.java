da4888bda7dfb9a1d356cae15aaf51aacf4edd20
Pulkit Jain <142732659+Pullkitt@users.noreply.github.com>
2025-02-12 00:46:22 +0530

    Update Person.java

--- src/exp5/Person.java
+++ src/exp5/Person.java
@@ -1,5 +1,55 @@
 package exp5;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Person {
+// Superclass
+abstract class Person {
+    abstract void performDuty();
+}
 
+// Subclass Student
+class Student extends Person {
+    @Override
+    void performDuty() {
+        System.out.println("Student is studying.");
+    }
+}
+
+// Subclass Teacher
+class Teacher extends Person {
+    @Override
+    void performDuty() {
+        System.out.println("Teacher is teaching.");
+    }
+}
+
+public class SchoolManagementSystem {
+    public static void main(String[] args) {
+        // Create a list of Person references
+        List<Person> people = new ArrayList<>();
+
+        // Add instances of Student and Teacher
+        people.add(new Student());
+        people.add(new Teacher());
+        people.add(new Student());
+        people.add(new Teacher());
+        people.add(new Student());
+
+        // Iterate through the list and call performDuty()
+        for (Person person : people) {
+            person.performDuty();
+        }
+
+        Person[] peopleArray = new Person[5];
+        peopleArray[0] = new Student();
+        peopleArray[1] = new Teacher();
+        peopleArray[2] = new Student();
+        peopleArray[3] = new Teacher();
+        peopleArray[4] = new Student();
+
+        System.out.println("\nDemonstrating with an array:");
+        for(Person person : peopleArray){
+            person.performDuty();
+        }
+    }
 }
